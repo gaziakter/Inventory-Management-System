f3b538c609fba4828d3680132bd22980a3cb8b3f
Gazi Akter <gaziakterpro@gmail.com>
2022-10-11 07:28:33 +0600

    insert multiple image

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\About;
+use App\Models\MultiImage;
 use Image;
+use Illuminate\Support\Carbon;
 
 class AboutController extends Controller
 {
@@ -65,4 +67,34 @@ class AboutController extends Controller
         $aboutpage = About::find(1);
         return view('frontend.about_page', compact('aboutpage'));
      }
+
+     public function AboutMultiImage(){
+        return view('admin.about_page.multiimage');
+     }
+
+
+    public function StoreMultiImage(Request $request){
+
+        $image = $request->file('multi_image');
+
+        foreach($image as $multi_image){
+            $name_gen = hexdec(uniqid()).'.'.$multi_image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($multi_image)->resize(220, 220)->save('upload/multi/'.$name_gen);
+            $save_url = 'upload/multi/'.$name_gen;
+    
+            MultiImage::insert([
+                'multi_image' => $save_url,
+                'created_at' => Carbon::now()
+            ]);             
+        }
+    
+        $notification = array(
+        'message' => 'Multi Image Inserted Successfully', 
+        'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }
 }
---
+++ resources/views/admin/about_page/multiimage.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.admin_master')
+@section('admin')
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-10">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">About Multi Image </h4>
+                        <hr>
+                        <form method="POST" action="{{route('store.mult.image')}}" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="col-sm-2 col-form-label">Add Multi Image</label>
+                                <div class="col-sm-10">
+                                    <input name="multi_image[]" multiple="" class="form-control" type="file" id="input_profile_picture">
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <img id="show_picture" src="{{(!empty($aboutpage->about_image))? url($aboutpage->about_image):url('upload/no_image.png')}}" alt="avatar-5" class="rounded avatar-lg">
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <input class="btn btn-info btn-rounded waves-effect waves-light" type="submit" value="Add Multi Image">
+                                </div>
+                            </div>
+                            <!-- end row -->
+                        </form>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#input_profile_picture').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#show_picture').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        })
+    });
+</script>
+
+@endsection
