a5e31899cd88953c6de138dfb4e6207f25fcc074
Gazi Akter <gaziakterpro@gmail.com>
2022-11-14 21:48:48 +0600

    updated multi image

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -94,7 +94,7 @@ class AboutController extends Controller
         'alert-type' => 'success'
         );
 
-        return redirect()->back()->with($notification);
+        return redirect()->route('all.multi.image')->with($notification);
 
     }
 
@@ -102,4 +102,33 @@ class AboutController extends Controller
         $allMultiImage = MultiImage::all();
         return view('admin.about_page.all_multiimage', compact('allMultiImage'));
     } // end function AllMultiImage
+
+    public function EditMultiImage($id){
+        $editMultiImage = MultiImage::findOrFail($id);
+        return view('admin.about_page.edit_multi_image', compact('editMultiImage'));
+    } 
+
+    public function UpdateMultiImage(Request $request){
+        $multi_image_id = $request->id;
+
+        if ($request->file('multi_image')) {
+            $image = $request->file('multi_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($image)->resize(220, 220)->save('upload/multi/'.$name_gen);
+            $save_url = 'upload/multi/'.$name_gen;
+
+            MultiImage::findOrFail($multi_image_id)->update([
+                'multi_image' => $save_url,
+
+            ]); 
+            $notification = array(
+            'message' => 'Multi Image Updated Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.multi.image')->with($notification);
+
+        }
+    }
 }
