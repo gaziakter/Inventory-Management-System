6f80d5225a58927b88532bc12e12894142399b29
Gazi Akter <gaziakterpro@gmail.com>
2022-11-17 08:23:33 +0600

    delete portfolio

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -105,4 +105,19 @@ class PortfolioController extends Controller
         } // end Else
 
      } // End Method 
+
+     public function DeletePortfolio($id){
+        $portfolio = Portfolio::findOrFail($id);
+        $img = $portfolio->portfolio_image;
+        unlink($img);
+
+        Portfolio::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Portfolio Deleted Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
 }
